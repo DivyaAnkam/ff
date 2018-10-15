2d057489a7c315de4a4bbe7e8dd6e298082a1be8
DivyaAnkam <44075302+DivyaAnkam@users.noreply.github.com>
2018-10-15 17:03:14 -0400

    good a1 c1

--- arcticLake.py
+++ arcticLake.py
@@ -18,15 +18,18 @@ Y3 = arctic['clay']
 X = arctic['depth']
 
 [[a1],[c1]]= mydef.mom(Y1)
-[[r1],[e1]] = mydef.Gg(a1,c1,Y1)
-#[[a1],[c1]] = [[a1],[c1]] - [[r1],[e1]]
-a1 = a1 - r1
-c1 = c1 - e1
+count = 0
+while 1:
+  [[r1],[e1]] = mydef.Gg(a1,c1,Y1)
+  #[[a1],[c1]] = [[a1],[c1]] - [[r1],[e1]]
+  a1 = a1 - r1
+  c1 = c1 - e1
+  print(count,"\n")
+  count += 1
+  if (a1 <= 0 or c1<= 0 or abs(r1) < 0.01 or abs(e1) < 0.01) :
+      break
 
-print("Divya1")
+print(a1," ",c1,"\n",r1," ",e1)
 
-[[r1],[e1]] = mydef.Gg(a1,c1,Y1)
-a1 = a1 - r1
-c1 = c1 - e1
 
 print("Divya21")
